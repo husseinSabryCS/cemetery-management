b0f7c2923d658b05da5468268d163fc49f1a1185
husseinSabryCS <sabryhusseinhussein@gmail.com>
2024-10-06 00:11:29 +0300

    add rest password auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const RestPasswordController = require('../controllers/RestPasswordController');
+
 const authenticateAndAuthorize = require('../Middleware/authMiddleware');
 router.post('/login', authController.login);
-// إنشاء مستخدم جديد, authenticateAndAuthorize(['manager'])
-router.post('/register',authController.createUser);
-router.post('/update-password',authController.updatePasswordAndSendEmail);
+// إنشاء مستخدم جديد,
+router.post('/register', authenticateAndAuthorize(['manager']),authController.createUser);
+router.post('/update-password', authenticateAndAuthorize(['manager']),authController.updatePasswordAndSendEmail);
 module.exports = router;
