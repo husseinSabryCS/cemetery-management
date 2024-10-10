f71e14f5fc602d0e44e779cab60fddd6f63c775a
husseinSabryCS <sabryhusseinhussein@gmail.com>
2024-10-11 00:08:46 +0300

    test3

--- routes/suggestionRoutes.js
+++ routes/suggestionRoutes.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const authenticateAndAuthorize = require('../Middleware/authMiddleware');
-const SuggestionController = require('../controllers/suggestionController.js');
+// const express = require('express');
+// const authenticateAndAuthorize = require('../Middleware/authMiddleware');
+// const SuggestionController = require('../controllers/suggestionController.js');
 
-const router = express.Router();
-// const {createSuggestion,
-//     getAllSuggestions,
-//     getSuggestionsByStatus,
-//     updateSuggestionStatus,
-//     getApprovedSuggestions
-//   } =require('../controllers/SuggestionController');
+// const router = express.Router();
+// // const {createSuggestion,
+// //     getAllSuggestions,
+// //     getSuggestionsByStatus,
+// //     updateSuggestionStatus,
+// //     getApprovedSuggestions
+// //   } =require('../controllers/SuggestionController');
 
 
-// 1. إضافة اقتراح جديد
-router.post('/', SuggestionController.createSuggestion);
+// // 1. إضافة اقتراح جديد
+// router.post('/', SuggestionController.createSuggestion);
 
-// 2. جلب جميع الاقتراحات
-router.get('/',authenticateAndAuthorize(['manager']) , SuggestionController.getAllSuggestions);
-router.get('/Approved', SuggestionController.getApprovedSuggestions);
+// // 2. جلب جميع الاقتراحات
+// router.get('/',authenticateAndAuthorize(['manager']) , SuggestionController.getAllSuggestions);
+// router.get('/Approved', SuggestionController.getApprovedSuggestions);
 
-// 3. جلب الاقتراحات بناءً على حالتها
-router.get('/status/:status',authenticateAndAuthorize(['manager']),SuggestionController.getSuggestionsByStatus);
+// // 3. جلب الاقتراحات بناءً على حالتها
+// router.get('/status/:status',authenticateAndAuthorize(['manager']),SuggestionController.getSuggestionsByStatus);
 
-// 4. تحديث حالة اقتراح (للإدمن)
-router.put('/status/:id',authenticateAndAuthorize(['manager']) ,SuggestionController.updateSuggestionStatus);
+// // 4. تحديث حالة اقتراح (للإدمن)
+// router.put('/status/:id',authenticateAndAuthorize(['manager']) ,SuggestionController.updateSuggestionStatus);
 
-module.exports = router;
+// module.exports = router;
